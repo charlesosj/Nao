5f8ece969b4db9b1e920aada0e2029c12135c354
vainmouse <christhapy@gmail.com>
2016-07-19 13:38:28 +0100

    behaviors.py automatically runs the launchfile

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-LAUNCH GAZEBO SIMULATION
-roslaunch gazebo_naoqi_control nao_gazebo.launch
+
 
 
 
 Everything launched from the code folder
 
-Behaviors
+Behaviors.py
+when run robot will stand up and  enable all joints.. on exit will sit down and disable joints
 python behaviors.py 10.18.12.56 (Robot IP)
 rostopic pub -1 /nao_behavior/run_behavior std_msgs/String -- 'System/animations/Stand/Emotions/Neutral/Hello_1'
 
@@ -13,12 +13,20 @@ rostopic pub -1 /nao_behavior/run_behavior std_msgs/String -- 'System/animations
 
 RVIZ Display
 http://wiki.ros.org/nao/Tutorials/Getting-Started
-rosrun nao.launch nao_ip:= 10.18.12.56
-
-rosrun rviz rviz -d ~/catkin_ws/src/nao_robot/nao_description/config/urdf.rviz
-
-
+1.rosrun nao.launch nao_ip:= 10.18.12.56
+2.rosrun rviz rviz -d ~/catkin_ws/src/nao_robot/nao_description/config/urdf.rviz
 
 
 Notes
+can use choreograph to wake up and make the Nao sit down but you can also call the following after nao.launch is called to enable and disable stiffness (behaviors.py does this for you too'
+
+rosservice call /body_stiffness/disable "{}"
+rosservice call /body_stiffness/enable "{}"
+
+
+
+MY notes
 roslaunch nao_bringup nao_full.launch nao_ip:=10.18.12.56 network_interface:=wlan0
+
+LAUNCH GAZEBO SIMULATION
+roslaunch gazebo_naoqi_control nao_gazebo.launch
--- code/behaviors.py
+++ code/behaviors.py
@@ -2,6 +2,11 @@ import sys
 import time
 import rospy
 from std_msgs.msg import String
+import almath
+import subprocess #running ros
+import xmlrpclib
+import os
+
 
 from naoqi import ALProxy
 
@@ -9,13 +14,29 @@ class NaoBehavior:
     def __init__(self,robotIP):
         # Create proxy to ALBehaviorManager
         self.managerProxy = ALProxy("ALBehaviorManager", robotIP, 9559)
-        rospy.Subscriber("/nao_behavior/run_behavior", String, self.launchAndStopBehavior)
-        rospy.Subscriber("/nao_behavior/get_behaviors", String, self.getBehaviors)
+        self.motionProxy  = ALProxy("ALMotion", robotIP, 9559)
+        rospy.Subscriber("/nao_behavior/run_behavior", String, self.run_callback)
+        rospy.Subscriber("/nao_behavior/stop_behavior", String, self.stop)
+        self.wakeup()
 
-    def launchAndStopBehavior(self, data):
+    def run_callback(self,data):
+        if data.data == 'wakeup':
+            self.wakeup()
+            return
+        if data.data == 'rest':
+            self.rest()
+            return
+        if data.data == "help":
+            self.getBehaviors()
+            #  self.Action(data.data)
+            return
+
+        self.launchBehavior(data.data)
+
+
+    def launchBehavior(self, behaviorName):
         ''' Launch and stop a behavior, if possible. '''
-        rospy.loginfo(data.data)
-        behaviorName = data.data
+
         # Check that the behavior exists.
         if (self.managerProxy.isBehaviorInstalled(behaviorName)):
 
@@ -34,17 +55,74 @@ class NaoBehavior:
             return
 
 
-    def stop(self):
-            # Stop the behavir.
-            if (self.managerProxy.isBehaviorRunning(behaviorName)):
-                self.managerProxy.stopBehavior(behaviorName)
+    def stop(self,behaviorName):
+            # Stop the behavior.
+            if (self.managerProxy.isBehaviorRunning(behaviorName.data)):
+                self.managerProxy.stopBehavior(behaviorName.data)
+                rospy.loginfo("Behavior stopped.")
+
                 time.sleep(1.0)
             else:
                 rospy.loginfo("Behavior is already stopped.")
 
+    def wakeup(self):
+        self.motionProxy.wakeUp()
+    def rest(self):
+        self.motionProxy.rest()
+        self.motionProxy.stiffnessInterpolation("Body", 0, 0.5)
+
+   #just some movement test
+    def Action(self,action):
+        # Define The Initial Position for the upper body
+        HeadYawAngle = + 0.0
+        HeadPitchAngle = + 0.0
+        ShoulderPitchAngle = +80.0
+        ShoulderRollAngle = +20.0
+        ElbowYawAngle = -80.0
+        ElbowRollAngle = -60.0
+        WristYawAngle = + 0.0
+        HandAngle = + 0.0
+        # Define legs position
+        kneeAngle = +40.0
+        torsoAngle = + 0.0  # bend the torso
+        spreadAngle = + 0.0  # spread the legs
+
+        # set joint angles for standing
+        if action != 'me':
+            Head = [HeadYawAngle, HeadPitchAngle]
+
+            LeftArm = [ShoulderPitchAngle, +ShoulderRollAngle, +ElbowYawAngle, +ElbowRollAngle, WristYawAngle,
+                       HandAngle]
+            RightArm = [ShoulderPitchAngle, -ShoulderRollAngle, -ElbowYawAngle, -ElbowRollAngle, WristYawAngle,
+                        HandAngle]
+
+            LeftLeg = [0.0,  # hipYawPitch
+                       spreadAngle,  # hipRoll
+                       -kneeAngle / 2 - torsoAngle,  # hipPitch
+                       kneeAngle,  # kneePitch
+                       -kneeAngle / 2,  # anklePitch
+                       -spreadAngle]  # ankleRoll
+            RightLeg = [0.0, -spreadAngle, -kneeAngle / 2 - torsoAngle, kneeAngle, -kneeAngle / 2, spreadAngle]
 
 
-    def getBehaviors(self, data):
+            # Gather the joints together
+        pTargetAngles = Head + LeftArm + LeftLeg + RightLeg + RightArm
+
+        # Convert to radians
+        pTargetAngles = [x * almath.TO_RAD for x in pTargetAngles]
+
+        # ------------------------------ send stiffness -----------------------------
+        self.motionProxy.stiffnessInterpolation("Body", 1.0, 0.5)
+
+        # ------------------------------ send the commands -----------------------------
+        # We use the "Body" name to signify the collection of all joints
+        pNames = "Body"
+        # We set the fraction of max speed
+        pMaxSpeedFraction = 0.2
+        # Ask motion to do this with a blocking call
+        self.motionProxy.angleInterpolationWithSpeed(pNames, pTargetAngles, pMaxSpeedFraction)
+
+    def getBehaviors(self):
         ''' Know which behaviors are on the robot '''
 
         names = self.managerProxy.getInstalledBehaviors()
@@ -54,13 +132,22 @@ class NaoBehavior:
        # rospy.loginfo(names)
 
         #names = managerProxy.getRunningBehaviors()
+    def on_shutdown(self):
+        self.rest()
 
+def check_ros():
+    # check if ros is running, if not start it
+    try:
+        m = xmlrpclib.ServerProxy(os.environ['ROS_MASTER_URI'])
+        code, msg, val = m.getSystemState('/script')
+    except:
+        roscore = subprocess.Popen('roscore')
+        time.sleep(1)  # wait a bit to be sure the roscore is really launched
+def launch_nodes():
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = path + '/nao.launch nao_ip:=10.18.12.56'
+    subprocess.Popen('roslaunch '+ path, shell=True)
 
-
-def main(robotIP, behaviorName):
-
-
-  getBehaviors(managerProxy)
 
 
 
@@ -68,13 +155,22 @@ def main(robotIP, behaviorName):
 
 if __name__ == "__main__":
 
-  if (len(sys.argv) < 1):
+  if (len(sys.argv) < 2):
     print "Usage python behaviors.py 10.18.12.56 "
     sys.exit(1)
+
+  check_ros()
   rospy.init_node('NaoBehavior', anonymous=True)
+  launch_nodes()
+
 
   app = NaoBehavior(sys.argv[1])
   rospy.spin()
+
+  #what to do on shutdown
+  rospy.on_shutdown(app.on_shutdown)
+
+
 #commands
 #rostopic pub -1 /nao_behavior/run_behavior std_msgs/String -- 'System/animations/Stand/Emotions/Neutral/Hello_1'
 #rostopic pub -1 /nao_behavior/get_behaviors std_msgs/String -- 'hi'
